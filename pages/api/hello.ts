d181594d2e1c5022f56e0de35ff4ac01ba6685de
João Victor <reflixplayshd@gmail.com>
2022-04-04 12:49:11 -0300

    implementing NextAuth

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  id: number;
+  title: string;
+  channel: string;
+  views: string;
+  date: string;
+  avatar: string;
+  thumb: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    id: sexo,
+    title: 'Elden Ring: Começando bem',
+    channel: 'Filipe Ramos',
+    views: '385 mil visualizações',
+    date: 'há 2 semana',
+    avatar: `path/filipeRamos.jpg`,
+    thumb: `path1.jpg`,
+  });
 }
+
+const sexo = 0;
